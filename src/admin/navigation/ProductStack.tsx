e54de27307b119e14f28d1fc3769a4e7bd5edfa3
adde <almedawer.adnan@gmail.com>
2025-07-17 11:20:13 +0200

    Refactors product navigation to use sub-tabs.

--- src/admin/navigation/ProductStack.tsx
+++ src/admin/navigation/ProductStack.tsx
@@ -7,17 +7,15 @@
  * It contains all screens related to managing products and categories.
  */
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { colors } from '@/shared/styles/colors';
-
-// Import all the relevant screens for this module
-import ProductManagementScreen from '../screens/productManagement/ProductManagementScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import {colors} from '@/shared/styles/colors';
 import CategoryListScreen from '../screens/productManagement/CategoryListScreen';
 import CreateCategoryScreen from '../screens/productManagement/CreateCategoryScreen';
 import EditCategoryScreen from '../screens/productManagement/EditCategoryScreen';
 import ProductListScreen from '../screens/productManagement/ProductListScreen';
 import CreateProductScreen from '../screens/productManagement/CreateProductScreen';
 import EditProductScreen from '../screens/productManagement/EditProductScreen';
+import ProductSubTabNavigator from "@/admin/navigation/ProductSubTabNavigator";
 
 const Stack = createStackNavigator();
 
@@ -34,13 +32,14 @@ const ProductStack = () => {
                 },
             }}
         >
-            <Stack.Screen name="ProductManagement" component={ProductManagementScreen} options={{ title: 'Product Management' }} />
-            <Stack.Screen name="CategoryList" component={CategoryListScreen} options={{ title: 'Categories' }} />
-            <Stack.Screen name="CreateCategory" component={CreateCategoryScreen} options={{ title: 'Create Category' }} />
-            <Stack.Screen name="EditCategory" component={EditCategoryScreen} options={{ title: 'Edit Category' }} />
-            <Stack.Screen name="ProductList" component={ProductListScreen} options={{ title: 'Products' }} />
-            <Stack.Screen name="CreateProduct" component={CreateProductScreen} options={{ title: 'Create Product' }} />
-            <Stack.Screen name="EditProduct" component={EditProductScreen} options={{ title: 'Edit Product' }} />
+            <Stack.Screen name="ProductSubTab" component={ProductSubTabNavigator}
+                          options={{headerShown: false}}/>
+            <Stack.Screen name="CategoryList" component={CategoryListScreen} options={{title: 'Categories'}}/>
+            <Stack.Screen name="CreateCategory" component={CreateCategoryScreen} options={{title: 'Create Category'}}/>
+            <Stack.Screen name="EditCategory" component={EditCategoryScreen} options={{title: 'Edit Category'}}/>
+            <Stack.Screen name="ProductList" component={ProductListScreen} options={{title: 'Products'}}/>
+            <Stack.Screen name="CreateProduct" component={CreateProductScreen} options={{title: 'Create Product'}}/>
+            <Stack.Screen name="EditProduct" component={EditProductScreen} options={{title: 'Edit Product'}}/>
         </Stack.Navigator>
     );
 };
