af9de2283d18c3bb7f6ac3d004ec5fa34ad482b3
adde <almedawer.adnan@gmail.com>
2025-07-25 17:54:20 +0200

    Adds image upload support to product creation.

--- src/admin/screens/productManagement/CreateProductScreen.tsx
+++ src/admin/screens/productManagement/CreateProductScreen.tsx
@@ -13,35 +13,17 @@ import AppButton from '../../../shared/components/common/AppButton';
 import ErrorText from '../../../shared/components/common/ErrorText';
 import ProductDetailForm from '../../components/productManagement/ProductDetailForm';
 import {parseApiError} from '@/shared/utils/errorHandler';
+import LoadingSpinner from "@/shared/components/common/LoadingSpinner";
 
 const CreateProductScreen = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [form, setForm] = useState<Partial<NewProduct>>({
-        productName: '',
-        productDescription: '',
-        sku: '',
-        upc: '',
-        hsn: '',
-        image: '',
-        categoryId: '',
-        price: 0,
-        quantity: 0,
-        dateAvailable: new Date().toISOString().split('T')[0], // Default to today
-        status: 1,
-        sortOrder: 0,
-    });
     const navigation = useNavigation();
-
-    const handleInputChange = (name: keyof NewProduct, value: string | number) => {
-        setForm(prev => ({...prev, [name]: value}));
-    };
-
-    const handleCreateProduct = async () => {
+    const handleCreateProduct = async (values: NewProduct) => {
         setLoading(true);
         setError(null);
         try {
-            const response = await createProductApi(form as NewProduct);
+            const response = await createProductApi(values);
             if (response.data && response.data.status === 1) {
                 Alert.alert('Success', 'Product created successfully!', [
                     {text: 'OK', onPress: () => navigation.goBack()},
@@ -55,13 +37,18 @@ const CreateProductScreen = () => {
             setLoading(false);
         }
     };
-
+    if (loading) {
+        return <LoadingSpinner/>;
+    }
     return (
         <SafeAreaView style={styles.safeArea}>
             <ScrollView style={styles.container}>
-                <ProductDetailForm form={form} onInputChange={handleInputChange}/>
+                <ProductDetailForm
+                    initialValues={{}}
+                    onSubmit={handleCreateProduct}
+                    isEditMode={false}
+                />
                 {error && <ErrorText message={error}/>}
-                <AppButton title="Create Product" onPress={handleCreateProduct} loading={loading}/>
             </ScrollView>
         </SafeAreaView>
     );
