3da976cccec231ec0b4337b883525d34fdb3aa8b
adde <almedawer.adnan@gmail.com>
2025-08-01 19:02:23 +0200

    Replaces AppNavigator with RootNavigator in App.tsx.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import {AuthProvider} from './src/shared/state/AuthContext';
-import AppNavigator from './src/admin/navigation/AppNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'expo-status-bar';
+import RootNavigator from "@/shared/navigation/RootNavigator";
 
 export default function App() {
     return (
         <AuthProvider>
             <NavigationContainer>
-                {/* We can later add logic here to decide whether to show
-            the Admin, Seller, or Storefront app */}
-                <AppNavigator/>
+                <RootNavigator />
                 <StatusBar style="auto"/>
             </NavigationContainer>
         </AuthProvider>
