73425a2b48d5cc38d0602773e344038aac653a52
adde <almedawer.adnan@gmail.com>
2025-08-05 19:14:40 +0200

    Configure app with environment variables.

---
+++ app.config.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+export default {
+  "expo": {
+    "name": "spurt2",
+    "slug": "spurt2",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "edgeToEdgeEnabled": true
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "APP_VARIANT": process.env.APP_VARIANT,
+    }
+  }
+}
